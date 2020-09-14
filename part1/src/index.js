7c28050ea44edc2db58bfdbb1e548f33dd80d443
Rifki F. Akbar <47451911+kikibanner@users.noreply.github.com>
2020-09-15 05:19:20 +0700

    part1- react basics

---
+++ part1/src/index.js
@@ -0,0 +1,117 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+// Membuat components
+// nama components harus kapital
+// PROPS = digunakan untuk mempass data kedalam components
+
+const Header = (props) => {
+  console.log(props)
+  return(
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.part} {props.exercises}</p>
+    </>
+  )
+}
+
+const Content = (props) => { 
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
+    </div>
+  )
+}
+
+// 10,7,14
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {10 + 7 + 14}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // const-definitions
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part1={parts[0].name} exercises1={parts[0].exercises} part2={parts[1].name} exercises2={parts[1].exercises} part3={parts[2].name} exercises3={parts[2].exercises}/>
+      <Total />
+    </div>
+  )
+}
+
+const Display = (props) => {
+  return (
+  <div>Counter: {props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Hello = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  return(
+    <div>
+      <Display counter={counter} />
+      <Button 
+        handleClick={increaseByOne}
+        text='tambah'
+      />
+      <Button 
+        handleClick={setToZero}
+        text='reset'
+      />
+      <Button 
+        handleClick={decreaseByOne}
+        text='kurang'
+      />
+    </div>
+  )
+} 
+
+ReactDOM.render(<Hello />, document.getElementById('joss'))
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+
+
+
+
