d0087cc024ebc0e68b746dd2491c70b55b4cef1e
Rifki F. Akbar <47451911+kikibanner@users.noreply.github.com>
2020-09-26 11:38:01 +0700

    part 2b mantab

--- part2/sanboxpart2/src/App.js
+++ part2/sanboxpart2/src/App.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
-    return (
-      <div>
-        <h1>Notes</h1>
-        <ul>
-          {notes.map(note =>
-            <Note key={note.id} note={note} />)}
-        </ul>
-      </div>
-    )
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('tombol ditekan', event.target)
   }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note =>
+          <Note key={note.id} note={note} />)}
+      </ul>
+      <form onSubmit={addNote}>
+          <input />
+          <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
   
 export default App
