bf473e8f0410b33ecb8ba39b57e0db9aa1b23f5a
Rifki F. Akbar <47451911+kikibanner@users.noreply.github.com>
2020-10-24 22:31:16 +0700

    pard 2d done

--- part2/sanboxpart2/src/App.js
+++ part2/sanboxpart2/src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -18,22 +19,45 @@ const App = () => {
   }
 
   useEffect(hook, [])
-  
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, important: !note.important}
+
+    noteService
+      .update(id, changedNote).then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from the server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
   console.log('nge-render sebanyak', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault() //method untuk memprevent submitting form scr default seng garakno page ngereload
     const noteObject = { //buat opject baru, contenntnya ambil dari setNotes() di concat notes nya
       content: newNote,
-      date: new Date().toISOString(),
+      date: new Date(),
       important: Math.random() < 0.5, // 50% chance menjadi important (true or false, 1 or 0)
-      id: notes.length + 1,
     }
 
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        setNotes(notes.concat(response.data))
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
         setNewNote('')
       })
   }
@@ -53,7 +77,7 @@ const App = () => {
   the result variable will be set to the value of val1 if condition is true. 
   If condition is false, the result variable will be set to the value of val2.
   */
-  
+ 
   return (
     <div>
       <h1>Notes</h1>
@@ -64,7 +88,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note =>
-          <Note key={note.id} note={note} />)}
+          <Note 
+            key={note.id} 
+            note={note} 
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />)}
       </ul>
       <form onSubmit={addNote}>
           <input 
